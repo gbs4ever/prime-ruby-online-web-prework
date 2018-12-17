106ed9d93718a4e8e2178170ea5cb8af09981ba9
s greenbaum <gbs4ever2@gmail.com>
2018-12-17 20:35:02 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,22 @@
 def prime?(num)
-  if num < 0 or num == 0 or num == 1
-    return false
-  else
-    (2..num-1).to_a.all? do |possible_factor|
-      num % possible_factor != 0
+
+    if num <=1
+        return false
+
+   elsif num ==2 || num ==3
+     return true
     end
+n=2
+  while  (n <= num)
+
+  if num  % n ==0
+   return  false
+
+ else
+   return true
+
+end
+  n=n+1
   end
 end
-#Set N=2.
-#Divide P by N and find the remainder R.
-#If R is zero, P is not prime. Exit.
-#Set N=N+1.
-#If N is greater than or equal P, P is Prime. Exit.
-#Go to step 2.
+
